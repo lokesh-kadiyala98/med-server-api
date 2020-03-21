ddd1a7be765c55d2e541fd36568f9201aea1bcbc
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-03-21 21:13:02 +0530

    Added Feature: Routes for Pharma

--- paths/pharma.js
+++ paths/pharma.js
@@ -26,7 +26,24 @@ router.get('/get_unique_medicines', (req, res) => {
     })
 })
 
-router.get('/get_timeseries_data', (req, res) => {
+router.get('/get_unique_brands', (req, res) => {
+    MongoClient.connect(DBurl, (err, client) => {
+        if (err)
+            res.send({ error: 'Database Connection: Seems like something went wrong!!' })
+        else {
+            const db = client.db('med')
+            db.collection('medicines').distinct('brand', (err, items) => {
+                if(err)
+                    res.status(400).send(err)
+                else{
+                    res.status(200).send(items)
+                }
+            })
+        }
+    })
+})
+
+router.get('/get_medicine_timeseries_data', (req, res) => {
     MongoClient.connect(DBurl, (err, client) => {
         if (err)
             res.send({ error: 'Database Connection: Seems like something went wrong!!' })
@@ -35,7 +52,7 @@ router.get('/get_timeseries_data', (req, res) => {
             db.collection('medicines').aggregate(
                 [
                     {
-                        $match: { name: req.query.medicineName }
+                        $match: { name: req.query.toForecast }
                     },
                     {
                         $lookup: 
@@ -83,4 +100,61 @@ router.get('/get_timeseries_data', (req, res) => {
     })
 })
 
+
+router.get('/get_brand_timeseries_data', (req, res) => {
+    MongoClient.connect(DBurl, (err, client) => {
+        if (err)
+            res.send({ error: 'Database Connection: Seems like something went wrong!!' })
+        else {
+            const db = client.db('med')
+            db.collection('medicines').aggregate(
+                [
+                    {
+                        $match: { brand: req.query.toForecast }
+                    },
+                    {
+                        $lookup: 
+                        {
+                            from: 'sales',
+                            localField: '_id',
+                            foreignField: 'medicineId',
+                            as: 'sales'
+                        }
+                    },
+                    {
+                        $unwind:'$sales'
+                    },
+                    {
+                        $group: 
+                        {
+                            _id: { $substr: ['$sales.date', 3, 7] },
+                            count: { $sum: '$sales.units' }
+                        }
+                    },
+                    {
+                        $project: 
+                        {
+                            count: 1,
+                            month: { $substr: ['$_id', 0, 2] }, 
+                            year: { $substr: ['$_id', 3, 4] }
+                        }
+                    },
+                    {   
+                        $sort: 
+                        { 
+                            year: 1, 
+                            month: 1 
+                        } 
+                    }
+                ]
+            ).toArray((err, items) => {
+                if(err)
+                    res.status(400).send(err)
+                else{
+                    res.status(200).send(items)
+                }
+            })
+        }
+    })
+})
 module.exports = router
