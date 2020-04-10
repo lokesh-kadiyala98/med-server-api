bbcc4c112cfff4e448fca95d7ab8b30fb95203d1
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-04-10 22:45:38 +0530

    corona state wise data scrapper

--- paths/corona_stats.js
+++ paths/corona_stats.js
@@ -2,6 +2,8 @@ const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 const express = require('express')
 const router = express.Router()
+const cheerio = require('cheerio')
+const request = require('request')
 
 const DBurl = 'mongodb://localhost:27017';
 
@@ -23,6 +25,54 @@ router.get('/get_corona_data', (req, res) => {
             })
         }
     })
+})
+
+function updateData() {
+    return new Promise((resolve, reject) => {
+        request({
+            method: 'GET',
+            url: 'https://www.mohfw.gov.in/#state-data'
+        }, (err, res, body) => {
+            if (err) 
+                reject(err)
+            let $ = cheerio.load(body);
+            let states = [], cases = [], cured = [], deceased = []
+            $('.table-striped tbody tr td').each(function(i) {
+                if(i % 5 === 1)
+                    states.push($(this).text().trim())
+                else if(i % 5 === 2)
+                    cases.push($(this).text().trim())
+                else if(i % 5 === 3)
+                    cured.push($(this).text().trim())
+                else if(i % 5 === 4)
+                    deceased.push($(this).text().trim())
+            })
+            for (let index = 0; index < states.length - 1; index++) {
+                let obj = {
+                    state: states[index],
+                    cases: cases[index],
+                    cured: cured[index],
+                    deceased: deceased[index]
+                }
+                MongoClient.connect(DBurl, (err, client) => {
+                    if (err)
+                        reject({ error: 'Database Connection: Seems like something went wrong!!' })
+                    else {
+                        const db = client.db('med')
+                        db.collection('corona_state_wise_break_down').updateOne({state: states[index]}, {$set: obj}, (err, res) => {
+                            if (err) reject(err)
+                        })
+                    }
+                })
+            }
+            resolve({status: 200, message: 'No errors'})
+        })
+    })
+}
+
+router.get('/get_corona_state_wise_data', async (req, res) => {
+    const result = await updateData()
+    res.send(result)
 })
 
 module.exports = router
