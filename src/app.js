ea8abc85da24b182646c9f9ba21b48b38add1a5c
lokesh-kadiyala98 <lokesh.pandu1998@gmail.com>
2020-07-14 19:43:23 +0530

    Code Reafactored: file structure

--- src/app.js
+++ src/app.js
@@ -9,14 +9,14 @@ const PORT = process.env.PORT || 5000
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-var disease_symptoms = require('../paths/disease_symptoms')
-var users = require('../paths/users')
-var kyh = require('../paths/kyh')
-var pharma = require('../paths/pharma')
-var corona_stats = require('../paths/corona_stats')
+var disease_symptoms = require('./paths/disease_symptoms')
+var users = require('./paths/users')
+var kyh = require('./paths/kyh')
+var pharma = require('./paths/pharma')
+var corona_stats = require('./paths/corona_stats')
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, app.js --production build</h1>')
+    res.send('<h1>Hello, World!</h1>')
 });
 
 app.use('/disease_symptoms', disease_symptoms)
